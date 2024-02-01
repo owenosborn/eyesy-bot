8aa07db96cc15d3c1425791a7250176f7de0995e
Owen Osborn <owenosborn@gmail.com>
2024-02-01 10:45:23 -0500

    add full eyesy api

--- eyesy-bot.py
+++ eyesy-bot.py
@@ -22,9 +22,33 @@ You say:
     def draw(screen, etc):
         pygame.draw.circle(screen,(255,0,0),(10,10),(10))
 
-setup() gets called once at the start and can be used to initialize things. draw() gets called every frame. Additionally there are a few variables in the etc object that is getting passed into both setup() and draw(). There are the variables etc.knob1, etc.knob2, etc.knob3, etc.knob4, and etc.knob5. These are all floating point numbers from 0-1 and can be used to control aspects of the graphics per the user request.  
+setup() gets called once at the start and can be used to initialize things. draw() gets called every frame. Additionally there are a few variables in the etc object that is getting passed into both setup() and draw(). The `etc` object contains the following:
 
-Format output for markdown. Describe the code briefly in a sentance or two.
+--- begin eyesy api
+
+-   `etc.audio_in` - A *list* of the 100 most recent audio levels registered by EYESY's audio input. The 100 audio values are stored as 16-bit, signed integers, ranging from a minimum of -32,768 to a maximum of +32,767.
+-   `etc.audio_trig` - A *boolean* value indicating a trigger event.    
+-	 `etc.xres` - A *float* of the horizontal component of the current output resolution. 
+-	 `etc.yres` - A *float* of the vertical component of the current output resolution. 
+-   `etc.knob1` - A *float* representing the current value of *Knob 1*. 
+-   `etc.knob2` - A *float* representing the current value of *Knob 3*. 
+-   `etc.knob3` - A *float* representing the current value of *Knob 3*. 
+-   `etc.knob4` - A *float* representing the current value of *Knob 4*. 
+-   `etc.knob5` - A *float* representing the current value of *Knob 5*. 
+-   `etc.lastgrab` - A **Pygame** *surface* that contains an image of the last taken screenshot taken (via the *Screenshot* button). This surface has dimensions of 1280 by 720, matching the full size of the screenshot.
+-   `etc.lastgrab_thumb` - A **Pygame** *surface* that contains a thumbnail image of the last taken screenshot taken (via the *Screenshot* button). This surface has dimensions of 128 by 72.
+-   `etc.midi_notes` - A *list* representing the 128 various MIDI note pitches. Each value in this list indicates whether that note is current on or not. For example, you could create a function that executes when “middle C” (MIDI note 60) is on with something like…
+
+    if etc.midi_notes[60] : yourFunctionHere()
+
+-   `etc.midi_note_new` - A *boolean* value indicating whether or not at least one new MIDI note on message was received since the last frame was drawn (via the `draw()`function).
+
+-   `etc.mode` - A *string* of the current mode’s name.
+-   `etc.mode_root` - A *string* of the file path to the current mode’s folder. This will return something like `/sdcard/Modes/Python/CurrentModeFolder`. This can be useful when images, fonts, or other resources need to be loaded from the mode’s folder. (The `setup()` function would be an appropriate place to do this.)
+
+--- end eyesy api
+
+Format output for markdown. Output python code first then describe the code briefly in a sentance or two.
 """
 first_message = "I am EYESY Bot! Type what you want to draw and I'll do my best to code it up!"
 context = [ {'role':'system', 'content': f"""{system_message}"""} ] 
